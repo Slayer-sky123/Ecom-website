b42e6729ada72f5209d0e1863117080e11a1db4b
Slayer-sky123 <sv181285@gmail.com>
2024-01-18 23:51:16 +0530

    Added Products

--- Shopping/src/Pages/Shop.jsx
+++ Shopping/src/Pages/Shop.jsx
@@ -1,9 +1,11 @@
 import Hero from "../Components/Hero/Hero"
+import Products from "../Components/Products/Products"
 
 const Shop = () => {
   return (
     <div className="pt-10">
       <Hero />
+      <Products />
     </div>
   )
 }
